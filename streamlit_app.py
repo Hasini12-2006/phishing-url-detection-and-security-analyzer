5a14c253f4e67d059119998c7db880cd2f8f7b1b
Hasini12-2006 <hasinireddy9030@gmail.com>
2026-04-02 21:30:23 +0530

    Update app.py, model.py, add streamlit_app.py, and rename utils.py

---
+++ streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title("🎈 My New App")
+st.title("Welcome to My Streamlit App!")
+st.write(
+    "Let's start building! for help and inspiration, check out the [Streamlit documentation](https://docs.streamlit.io/) and the [Streamlit community forum](https://discuss.streamlit.io/)."
+)
